e21405dd29fcc2a652f64997ba12c9fe15b9ff6d
Alexander Garichev <SeptimusVega@proton.me>
2025-11-04 23:44:42 +0300

    style(main.css): Add scrollbar customization

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,5 @@
 <template>
-  <div
-    class="bg-obsidian-900 flex min-h-svh w-full flex-col overflow-x-clip text-white"
-  >
+  <div class="flex min-h-svh w-full flex-col overflow-x-clip text-white">
     <LayoutHeader class="shrink-0" />
     <div class="grow">
       <slot />
