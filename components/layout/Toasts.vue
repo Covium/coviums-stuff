c06a419e5316544354b9211ce515f56b765ae6da
Alexander Garichev <SeptimusVega@proton.me>
2025-11-04 22:27:39 +0300

    style(Toasts): Adapt progress colors to Chrome

--- components/layout/Toasts.vue
+++ components/layout/Toasts.vue
@@ -26,7 +26,7 @@ function toastClass(type: string) {
       class="relative flex items-center justify-center gap-x-2 overflow-clip rounded-full border-2 border-fuchsia-700 bg-transparent px-4 py-2"
     >
       <progress
-        class="bg-obsidian-950 [&::-moz-progress-bar]:bg-obsidian-800 absolute -z-1 h-full w-full appearance-none"
+        class="bg-obsidian-950 [&::-webkit-progress-bar]:bg-obsidian-950 [&::-webkit-progress-value]:bg-obsidian-800 [&::-moz-progress-bar]:bg-obsidian-800 absolute -z-1 h-full w-full appearance-none"
         :max="toast.duration"
         :value="toast.duration - toast.elapsed"
       />
