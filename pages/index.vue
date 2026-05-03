7264fa8e486cd45796589fda7d02b09a1114b726
Alexander Garichev <SeptimusVega@proton.me>
2026-05-03 23:38:10 +0300

    style(Section): Add FullHD max width

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section
-    class="grid grid-cols-1 gap-y-4 p-4 sm:p-8 md:grid-cols-2 md:gap-4 xl:grid-cols-3"
+    class="mx-auto grid max-w-[1920px] grid-cols-1 gap-y-4 p-4 sm:p-8 md:grid-cols-2 md:gap-4 xl:grid-cols-3"
   >
     <ModsCollection />
     <ColorBlender />
