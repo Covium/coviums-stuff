732b66f024300a0fc2c7f4dc0f515c03c7b4e570
Alexander Garichev <SeptimusVega@proton.me>
2026-05-04 00:15:53 +0300

    style(Layout): Add background diagonals mask

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="flex min-h-svh w-full flex-col overflow-x-clip text-white">
     <LayoutHeader class="shrink-0" />
-    <div class="grow">
-      <slot />
+    <div class="relative grow overflow-hidden">
+      <div
+        class="pointer-events-none absolute inset-0 bg-white/3 mask-[url('/assets/icons/Diagonals.svg')] mask-size-[13px] mask-center mask-repeat"
+      />
+      <div class="relative z-0">
+        <slot />
+      </div>
     </div>
 
     <LayoutToasts />
