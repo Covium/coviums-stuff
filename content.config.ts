5ab1666c41c7dd697065b2cbc2e0599f3d43c7c3
Alexander Garichev <SeptimusVega@proton.me>
2026-05-06 22:57:39 +0300

    refactor(content.config.ts): Swap modSchema and collectionSchema definitions and update id type

--- content.config.ts
+++ content.config.ts
@@ -1,19 +1,19 @@
 import { defineContentConfig, defineCollection, z } from '@nuxt/content';
 
-export const modSchema = z.object({
-  name: z.string(),
-  description: z.string(),
-  link: z.string().url().optional(),
-  provider: z.string().optional(),
-  id: z.number().optional(),
-});
-
 export const collectionSchema = z.object({
   name: z.string(),
   description: z.string(),
   link: z.string().url().optional(),
   provider: z.string().optional(),
   id: z.string().optional(),
+});
+
+export const modSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+  link: z.string().url().optional(),
+  provider: z.string().optional(),
+  id: z.number().optional(),
 });
 
 export const gameSchema = z.object({
