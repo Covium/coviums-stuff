1d8a75edaf5e3ebb37e972273458bbca9ca37c68
Alexander Garichev <SeptimusVega@proton.me>
2026-05-06 21:42:11 +0300

    refactor: Run prettier through whole project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,19 @@
 // @ts-check
-import withNuxt from "./.nuxt/eslint.config.mjs";
-import vue from "eslint-plugin-vue";
+import withNuxt from './.nuxt/eslint.config.mjs';
+import vue from 'eslint-plugin-vue';
 
 export default withNuxt({
   rules: {
-    "vue/html-self-closing": [
-      "error",
+    'vue/html-self-closing': [
+      'error',
       {
         html: {
-          void: "always",
-          normal: "always",
-          component: "always",
+          void: 'always',
+          normal: 'always',
+          component: 'always',
         },
-        svg: "always",
-        math: "always",
+        svg: 'always',
+        math: 'always',
       },
     ],
   },
