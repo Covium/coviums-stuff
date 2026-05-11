87400d2cac589a8dba8a6f6e8b95d7b411874bca
Alexander Garichev <SeptimusVega@proton.me>
2026-05-11 16:44:41 +0300

    style(Link): Add classes for when the link's destination is undefined

--- components/common/Link.vue
+++ components/common/Link.vue
@@ -5,18 +5,27 @@ import { computed } from 'vue';
 const props = defineProps<NuxtLinkProps & { containerClass?: string }>();
 
 const nuxtLinkProps = computed(() => {
-  const { containerClass: _containerClass, prefetch, noPrefetch, ...rest } = props;
+  const {
+    containerClass: _containerClass,
+    prefetch,
+    noPrefetch,
+    ...rest
+  } = props;
   if (noPrefetch) return { ...rest, noPrefetch: true };
   return { ...rest, prefetch };
 });
 </script>
 
 <template>
-  <NuxtLink v-bind="nuxtLinkProps" class="inline">
+  <NuxtLink
+    v-bind="nuxtLinkProps"
+    class="inline"
+    :class="{ 'cursor-text text-gray-300! hover:text-gray-300!': !props.to }"
+  >
     <span class="inline-block min-w-0" :class="props.containerClass">
       <slot />
       <Icon
-        v-if="props.target === '_blank'"
+        v-if="props.to && props.target === '_blank'"
         name="ic:round-arrow-outward"
         size="0.75rem"
         class="ml-0.5 inline align-super"
