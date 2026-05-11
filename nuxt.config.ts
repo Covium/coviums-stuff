bb4e0067a71f0699300a4ceffe598f74f059cd63
Alexander Garichev <SeptimusVega@proton.me>
2026-05-11 15:49:45 +0300

    chore(nuxt.config): Remove unused favicon entry from configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,6 @@ export default defineNuxtConfig({
         lang: 'en',
       },
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
         {
           rel: 'icon',
           type: 'image/png',
